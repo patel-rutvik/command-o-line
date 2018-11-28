26527741a4ce6d8a4038d0270d426038045d0eb1
Kkill77 <34036324+Kkill77@users.noreply.github.com>
2018-11-27 21:08:39 -0700

    Add files via upload

--- characters.py
+++ characters.py
@@ -1,6 +1,6 @@
 import pygame
 import time
-display_width, display_height = 1000, 500
+display_width, display_height = 2000, 1000
 
 
 class Player(pygame.sprite.Sprite):
@@ -11,7 +11,6 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.transform.scale(self.image, (100,100))
         self.rect = self.image.get_rect()
         self.rect.y = 300
-        self.jumped = False
 
     def update(self):
         keys = pygame.key.get_pressed()
@@ -28,10 +27,10 @@ class Player(pygame.sprite.Sprite):
             Player.jump(self)
             if self.rect.y <= (300 - 150):
                 self.jumped = True
-        if (keys[pygame.K_UP] == False) or self.jumped == True:
+        if (keys[pygame.K_UP] == False and self.rect.y < 300):
+            self.jumped = True
+        if self.jumped == True:
             Player.gravity(self)
-            
-
 
     def moveRight(self):
         self.rect.x += 4
@@ -41,16 +40,19 @@ class Player(pygame.sprite.Sprite):
         self.rect.x -= 4
 
     def jump(self):
-        self.rect.y -= 5
+        self.rect.y -= 8
         
     def gravity(self):
-        self.rect.y += 5
+        self.rect.y += 8
 
 
 
+class Enemy(pygame.sprite.Sprite):
 
-
-
-
-
-
+    def __init__(self):
+        pygame.sprite.Sprite.__init__(self)
+        self.image = pygame.image.load("goomba.png")
+        self.image = pygame.transform.scale(self.image, (100,100))
+        self.rect = self.image.get_rect()
+        self.rect.y = 300
+        self.rect.x = 200
--- play_game.py
+++ play_game.py
@@ -8,15 +8,17 @@ green = (100, 200, 100)
 red = (200, 0, 0)
 yellow = (180, 180, 0)
 hoveryellow = (200, 200, 0)
-display_width, display_height = 1000, 500
+display_width, display_height = 2000, 1000
 clock = pygame.time.Clock()
 gameDisplay = pygame.display.set_mode((display_width, display_height))
 background = pygame.image.load('game_background.jpg')
-background = pygame.transform.scale(background, (1000, 500))
+background = pygame.transform.scale(background, (2000, 1000))
 
 all_sprites = pygame.sprite.Group()
 player = characters.Player()
 all_sprites.add(player)
+goomba = characters.Enemy()
+all_sprites.add(goomba)
 keys = pygame.key.get_pressed()
 
 
