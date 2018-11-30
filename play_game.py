03b403a7609b08b3de7f42fedb4bc4736d657d14
Kkill77 <34036324+Kkill77@users.noreply.github.com>
2018-11-29 19:47:14 -0700

    Add files via upload

--- play_game.py
+++ play_game.py
@@ -2,6 +2,7 @@ import pygame
 from util import displayText, button
 import characters
 
+QUIT = False
 black = (0, 0, 0)
 white = (255, 255, 255)
 green = (100, 200, 100)
@@ -19,8 +20,6 @@ playerGroup = pygame.sprite.Group()
 player = characters.Player()
 playerGroup.add(player)
 enemyGroup = pygame.sprite.Group()
-enemy = characters.Enemy()
-enemyGroup.add(enemy)
 bulletGroup = pygame.sprite.Group()
 keys = pygame.key.get_pressed()
 
@@ -28,42 +27,49 @@ keys = pygame.key.get_pressed()
 def playGame():
     play = True
 
-    while play:
-        gameDisplay.blit(background, (0, 0))
-        
-        if player.shot == True:
-            bullet = characters.Bullet(player.rect.x, player.rect.y, player.facing)
-            bulletGroup.add(bullet)
-        if pygame.sprite.groupcollide(bulletGroup, enemyGroup, True, False):
-            enemy.health -= bullet.damage
-        if enemy.alive == False:
-            enemy.remove(enemyGroup)
-        for bullet in bulletGroup.sprites():
-            if bullet.alive == False:
-                bulletGroup.remove(bullet)
-
-        #UPDATE
-        playerGroup.update()
-        enemyGroup.update()
-        bulletGroup.update()
-
-        #DRAW
-        playerGroup.draw(gameDisplay)
-        enemyGroup.draw(gameDisplay)
-        bulletGroup.draw(gameDisplay)
-
-        if (player.rect.x > display_width):
-            level_1()
-            player.rect.x = 0
-
-        #END PLAY
+    while play:     
+        logic(playerGroup, enemyGroup, bulletGroup)
         for event in pygame.event.get():
             if event.type == pygame.KEYDOWN:
                 if event.key == pygame.K_RETURN:
                     play = False
             if event.type == pygame.QUIT:
                 return True
-        pygame.display.update()
+        if (player.rect.x > display_width):
+            level_1()
+            print("Nice")
+
+def logic(playerGroup, enemyGroup, bulletGroup):
+    gameDisplay.blit(background, (0, 0))
+    print(bulletGroup.sprites())
+        
+    if player.shot == True:
+        bullet = characters.Bullet(player.rect.x, player.rect.y, player.facing)
+        bulletGroup.add(bullet)
+    for enemy in enemyGroup:
+        if pygame.sprite.spritecollideany(enemy, bulletGroup):
+            pygame.sprite.groupcollide(bulletGroup, enemyGroup, True, False)
+            enemy.health -= 10
+    for enemy in enemyGroup.sprites():
+        if enemy.alive == False:
+            enemy.remove(enemyGroup)
+    for bullet in bulletGroup.sprites():
+        if bullet.alive == False:
+            bulletGroup.remove(bullet)
+
+    #UPDATE
+    playerGroup.update()
+    enemyGroup.update()
+    bulletGroup.update()
+
+    #DRAW
+    playerGroup.draw(gameDisplay)
+    enemyGroup.draw(gameDisplay)
+    bulletGroup.draw(gameDisplay)
+
+    pygame.display.update()
+
+
 
 def level_1():
     enemy = characters.Enemy()
@@ -71,5 +77,16 @@ def level_1():
     level_bkgd = pygame.image.load('medium_background.png')
     level_bkgd = pygame.transform.scale(level_bkgd, (2000, 1000))
     level_1 = True
-    # while level_1:........
+    player.rect.x = 0
+    while level_1:
+        logic(playerGroup, enemyGroup, bulletGroup)
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_RETURN:
+                    play = False
+            if event.type == pygame.QUIT:
+                pygame.quit()
+        if (player.rect.x > display_width):
+            level_1 = False
+
 
