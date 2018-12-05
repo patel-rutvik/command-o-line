5fcb9f09e032ab631bc5a25535d244d0fa3d3b47
Rutvik Patel <43623225+patel-rutvik@users.noreply.github.com>
2018-12-04 22:22:39 -0700

    Add files via upload

---
+++ code/menu_screen.py
@@ -0,0 +1,58 @@
+import pygame
+from util import displayText, button
+import characters
+white = (255, 255, 255)
+hovergreen = (140, 240, 100)
+green = (140, 200, 100)
+hoverred = (255, 0, 0)
+red = (200, 0, 0)
+blue = (0, 0, 200)
+hoverblue = (0, 0, 255)
+black = (0, 0, 0)
+display_width, display_height = 2000, 1000
+clock = pygame.time.Clock()
+gameDisplay = pygame.display.set_mode((display_width, display_height))
+background = pygame.image.load('../images/game_background.jpg')
+background = pygame.transform.scale(background, (2000, 1000))
+button_size = int((display_width / display_height) * 25)
+# https://www.1001freefonts.com/retro-fonts-4.php 
+
+bad_guys = 3
+menuGroup = pygame.sprite.Group()
+menu_mainPlayer = characters.menugoodGuy()
+menuGroup.add(menu_mainPlayer)
+for i in range(bad_guys):
+    menu_badPlayer = characters.menubadGuy()
+    menuGroup.add(menu_badPlayer)
+
+
+def menuScreen(state):
+    gameDisplay.blit(background, (0, 0))
+    displayText("COMMAND-O-LINE", '../fonts/Antonio-Bold.ttf', 200, display_width / 2, (display_height / 5),
+                 white, 0)
+    play_state = button("Play game", '../fonts/Antonio-Regular.ttf', button_size, white, green, 
+                        hovergreen, display_width / 5, display_height - button_size, 50, "play")
+    tutorial_state = button("Tutorial", '../fonts/Antonio-Regular.ttf', button_size, white, blue,
+                             hoverblue, display_width / 2, display_height - button_size, 50, 
+                             "tutorial")
+    quit_state = button("Quit game", '../fonts/Antonio-Regular.ttf', button_size, white, red, hoverred,
+                        (4*display_width) / 5, display_height - button_size, 50, "quit")
+                    
+    menuGroup.update()
+    menuGroup.draw(gameDisplay)
+    if play_state != state:
+        #menuGroup.empty()
+        #menuGroup.update()
+        return play_state
+    elif quit_state != state:
+        #menuGroup.empty()
+        #menuGroup.update()
+        return quit_state
+    elif tutorial_state != state:
+        #menuGroup.empty()
+        #menuGroup.update()
+        return tutorial_state
+
+    #if state != button_state:
+     #   return button_state
+
---
+++ code/tutorial.py
@@ -0,0 +1,70 @@
+import pygame
+from util import displayText, button
+import characters
+
+black = (0, 0, 0)
+white = (255, 255, 255)
+green = (100, 200, 100)
+red = (200, 0, 0)
+hoverred = (220, 0, 0)
+yellow = (180, 180, 0)
+hoveryellow = (200, 200, 0)
+display_width, display_height = 2000, 1000
+clock = pygame.time.Clock()
+gameDisplay = pygame.display.set_mode((display_width, display_height))
+
+all_sprites = pygame.sprite.Group()
+player = characters.Player()
+all_sprites.add(player)
+
+background = pygame.image.load('../images/game_background.jpg')
+background = pygame.transform.scale(background, (2000, 1000))
+left_key = pygame.image.load('../images/left_key.png')
+left_key = pygame.transform.scale(left_key, (125, 125))
+right_key = pygame.image.load('../images/right_key.png')
+right_key = pygame.transform.scale(right_key, (125, 125))
+up_key = pygame.image.load('../images/up_key.png')
+up_key = pygame.transform.scale(up_key, (125, 125))
+character = pygame.image.load('../images/main_guy.png')
+character = pygame.transform.scale(character, (150,150))
+
+tutorialGroup = pygame.sprite.Group()
+tutorialPlayer = characters.tutorialGuy()
+tutorialGroup.add(tutorialPlayer)
+bulletGroup = pygame.sprite.Group()
+
+def tutorialScreen():
+    tutorial = True
+    while tutorial:
+        gameDisplay.blit(background, (0, 0))
+        gameDisplay.blit(character, ((display_width / 2) + 185, 180))
+        if tutorialPlayer.shot == True:
+            bullet = characters.Bullet(tutorialPlayer.rect.x, tutorialPlayer.rect.y, tutorialPlayer.facing, tutorialPlayer.location)
+            bulletGroup.add(bullet)
+            tutorialPlayer.canShoot = False
+        tutorialGroup.update()
+        bulletGroup.update()
+        bulletGroup.draw(gameDisplay)
+        tutorialGroup.draw(gameDisplay)
+        displayText("Tutorial", '../fonts/Antonio-Bold.ttf', 125, display_width / 2, 100, white, 0)
+        displayText("You will be controlling             ", '../fonts/Antonio-Regular.ttf', 65, (display_width / 2), 275, white, 0)
+        displayText("Roberto", '../fonts/Antonio-Bold.ttf', 30, (display_width / 2) + 250, 350, white, 0)
+        displayText("Press       on the keypad to move right", '../fonts/Antonio-Regular.ttf', 50, (display_width / 3), 475, white, 0)
+        displayText("Press       on the keypad to move left", '../fonts/Antonio-Regular.ttf', 50, (display_width / 3), 575, white, 0)
+        displayText("Press       on the keypad to jump", '../fonts/Antonio-Regular.ttf', 50, (display_width / 3), 675, white, 0)
+        displayText("Use the mouse to aim and shoot!", '../fonts/Antonio-Regular.ttf', 50, (display_width / 3), 775, white, 0)
+        tempState = button("Main Menu (m)", '../fonts/Antonio-Regular.ttf', 40, white, red, hoverred, 1875, 970, 25, False)
+        if tempState != None:
+            tutorial = tempState
+        gameDisplay.blit(right_key, ((display_width / 3) - 310, 385))
+        gameDisplay.blit(left_key, ((display_width / 3) - 215, 480))
+        gameDisplay.blit(up_key, ((display_width / 3) - 225, 635))
+        for event in pygame.event.get():
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_m:
+                    tutorial = False
+            if event.type == pygame.QUIT:
+                return True ### Quit does not work...
+        pygame.display.update()
+        clock.tick(60)
+
